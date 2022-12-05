7ffc5904709993e2bf6423e30cbe0d33232b1ef7
rmasonhx12 <rmasonh22@gmail.com>
2022-12-05 12:26:36 -0500

    add item component

--- src/components/Docs.vue
+++ src/components/Docs.vue
@@ -2,16 +2,21 @@
     <div>
         <h1>DocExpire</h1>
         <div v-for="item in items" v-bind:key="item.id">
-        <h3>{{item.name}}</h3>
-        <p>{{item.exp}}</p>
+       <!--  <h3>{{item.name}}</h3>
+        <p>{{item.exp}}</p> -->
+        <Item v-bind:item="item"/>
         </div>
     </div>
 </template>
 
 <script>
+import Item from './item.vue';
     export default {
         name: "DocsVue",
-        props: ["items"]
+        props: ["items"],
+        components: {
+            Item
+        }
     }
 </script>
 
---
+++ src/components/item.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h3>{{item.name}}</h3>
+        <p>{{item.exp}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"itemVue",
+        props: ["item"]
+    }
+</script>
+
+<style scoped>
+
+</style>
